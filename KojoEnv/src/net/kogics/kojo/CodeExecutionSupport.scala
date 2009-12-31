a4c089a75477395280841dfd54f3cd0cf3e243a1
pant lalit <pant.lalit@gmail.com>
2009-12-31 10:48:07 +0530

    Checking outstanding (mainly NB project related) changes

--- KojoEnv/src/net/kogics/kojo/CodeExecutionSupport.scala
+++ KojoEnv/src/net/kogics/kojo/CodeExecutionSupport.scala
@@ -96,7 +96,7 @@ class CodeExecutionSupport private extends core.CodeCompletionSupport {
     val msg = """Welcome to Kojo! 
     |* To access context-sensitive actions  ->  Right-click on (most) windows
     |* To Pan/Zoom within the Turtle window ->  Press the left/right mouse button and drag
-    |  * To Reset Pan and Zoom levels       ->  Resize the Turtle window
+    |  * To reset Pan and Zoom levels       ->  Resize the Turtle window
     |* To see a list of available commands  ->  Type help and press Ctrl+Enter in the script window
     |""".stripMargin
     
