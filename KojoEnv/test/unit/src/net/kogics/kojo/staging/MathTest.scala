a9cf98396faca018848689a8f6a0947f570a672b
Peter Lewerin <peter.lewerin@tele2.se>
2010-06-10 00:45:36 +0200

    More refactoring of the tests.  SimpleShapesTest broken again.

--- KojoEnv/test/unit/src/net/kogics/kojo/staging/MathTest.scala
+++ KojoEnv/test/unit/src/net/kogics/kojo/staging/MathTest.scala
@@ -15,102 +15,13 @@
 package net.kogics.kojo
 package staging
 
-//import org.junit.After
-//import org.junit.Before
 import org.junit.Test
 import org.junit.Assert._
-
-import java.util.concurrent.atomic.AtomicBoolean
-import java.util.concurrent.{CountDownLatch, TimeUnit}
-
-import net.kogics.kojo.core.RunContext
 
 import net.kogics.kojo.util._
 
 // cargo coding off CodePaneTest
-class MathTest extends KojoTestBase {
-
-  val fileStr = System.getProperty("nbjunit.workdir") + "../../../../../../../Kojo/build/cluster"
-  val file = new java.io.File(fileStr)
-  assertTrue(file.exists)
-  System.setProperty("netbeans.dirs", fileStr)
-
-  val runCtx = new RunContext {
-    val currOutput = new StringBuilder()
-    val success = new AtomicBoolean()
-    val error = new AtomicBoolean()
-
-    def inspect(obj: AnyRef) {}
-    def onInterpreterInit() {}
-    def showScriptInOutput() {}
-    def hideScriptInOutput() {}
-    def showVerboseOutput() {}
-    def hideVerboseOutput() {}
-    def reportRunError() {}
-    def readInput(prompt: String) = ""
-
-    def println(outText: String) = reportOutput(outText)
-    def reportOutput(lineFragment: String) {
-      currOutput.append(lineFragment)
-    }
-
-    def onInterpreterStart(code: String) {}
-    def clearOutput {currOutput.clear}
-    def getCurrentOutput: String  = currOutput.toString
-
-    def onRunError() {
-      error.set(true)
-      latch.countDown()
-    }
-    def onRunSuccess() {
-      success.set(true)
-      latch.countDown()
-    }
-    def onRunInterpError() {latch.countDown()}
-
-    def reportErrorMsg(errMsg: String) {
-      currOutput.append(errMsg)
-    }
-    def reportErrorText(errText: String) {
-      currOutput.append(errText)
-    }
-  }
-
-  val codeRunner = new xscala.ScalaCodeRunner(runCtx, SpriteCanvas.instance, geogebra.GeoGebraCanvas.instance.geomCanvas)
-  val pane = new javax.swing.JEditorPane()
-  val Delimiter = ""
-  var latch: CountDownLatch = _
-
-  def runCode() {
-    latch = new CountDownLatch(1)
-    codeRunner.runCode(pane.getText())
-    latch.await()
-  }
-
-  def scheduleInterruption() {
-    new Thread(new Runnable {
-        def run() {
-          Thread.sleep(1000)
-          codeRunner.interruptInterpreter()
-        }
-      }).start()
-  }
-
-  object Tester {
-    var resCounter = 0
-
-    def apply (cmd: String, s: String) = {
-      runCtx.clearOutput
-      val res = stripCrLfs(Delimiter) + "res" + resCounter + ": " + s
-      resCounter += 1
-      pane.setText(cmd)
-      runCtx.success.set(false)
-      runCode()
-      Utils.runInSwingThreadAndWait {  /* noop */  }
-      assertTrue(runCtx.success.get)
-      assertEquals(res, stripCrLfs(runCtx.getCurrentOutput))
-    }
-  }
+class MathTest extends StagingTestBase {
 
   @Test
   // lalit sez: if we have more than five tests, we run out of heap space - maybe
@@ -122,20 +33,20 @@ class MathTest extends KojoTestBase {
   //W
   //W{{{
   //Wconstrain(value, min, max)
-    Tester("Staging.constrain(-22, -13, 78)", "Double = -13.0")
-    Tester("Staging.constrain(-2, -13, 78)", "Double = -2.0")
-    Tester("Staging.constrain(22, -13, 78)", "Double = 22.0")
-    Tester("Staging.constrain(82, -13, 78)", "Double = 78.0")
+    Tester("Staging.constrain(-22, -13, 78)", Some("Double = -13.0"))
+    Tester("Staging.constrain(-2, -13, 78)", Some("Double = -2.0"))
+    Tester("Staging.constrain(22, -13, 78)", Some("Double = 22.0"))
+    Tester("Staging.constrain(82, -13, 78)", Some("Double = 78.0"))
   //W}}}
   //W
   //WConstrains _value_ to be no less than _min_ and no greater than _max_.
   //W
   //W{{{
   //Wnorm(value, low, high)
-    Tester("Staging.norm(-22, -13, 78).toFloat", "Float = -0.0989011")
-    Tester("Staging.norm(-2, -13, 78).toFloat", "Float = 0.12087912")
-    Tester("Staging.norm(22, -13, 78).toFloat", "Float = 0.3846154")
-    Tester("Staging.norm(82, -13, 78).toFloat", "Float = 1.043956")
+    Tester("Staging.norm(-22, -13, 78).toFloat", Some("Float = -0.0989011"))
+    Tester("Staging.norm(-2, -13, 78).toFloat", Some("Float = 0.12087912"))
+    Tester("Staging.norm(22, -13, 78).toFloat", Some("Float = 0.3846154"))
+    Tester("Staging.norm(82, -13, 78).toFloat", Some("Float = 1.043956"))
   //W}}}
   //W
   //WNormalizes _value_ to the range 0.0 -- 1.0, such that _value_ == _low_
@@ -145,10 +56,10 @@ class MathTest extends KojoTestBase {
   //W
   //W{{{
   //Wmap(value, min1, max1, min2, max2)
-    Tester("Staging.map(-22, -13, 78, 5, 20).toFloat", "Float = 3.5164835")
-    Tester("Staging.map(-2, -13, 78, 5, 20).toFloat", "Float = 6.8131866")
-    Tester("Staging.map(22, -13, 78, 5, 20).toFloat", "Float = 10.769231")
-    Tester("Staging.map(82, -13, 78, 5, 20).toFloat", "Float = 20.65934")
+    Tester("Staging.map(-22, -13, 78, 5, 20).toFloat", Some("Float = 3.5164835"))
+    Tester("Staging.map(-2, -13, 78, 5, 20).toFloat", Some("Float = 6.8131866"))
+    Tester("Staging.map(22, -13, 78, 5, 20).toFloat", Some("Float = 10.769231"))
+    Tester("Staging.map(82, -13, 78, 5, 20).toFloat", Some("Float = 20.65934"))
   //W}}}
   //W
   //WMaps _value_ from the range _low1_ -- _high1_ to the range
@@ -156,7 +67,7 @@ class MathTest extends KojoTestBase {
   //W
   //W{{{
   //Wsq(value)
-    Tester("Staging.sq(-8)", "Double = 64.0")
+    Tester("Staging.sq(-8)", Some("Double = 64.0"))
   //W}}}
   //W
   //WYields _value_ squared.
@@ -170,17 +81,15 @@ class MathTest extends KojoTestBase {
   //W
   //W{{{
   //Wlerp(low, high, value)
-    Tester("Staging.lerp(-22, -13, 0.0).toFloat", "Float = -22.0")
-    Tester("Staging.lerp(-22, -13, 0.3).toFloat", "Float = -19.3")
-    Tester("Staging.lerp(-22, -13, 0.5).toFloat", "Float = -17.5")
-    Tester("Staging.lerp(-22, -13, 1.0).toFloat", "Float = -13.0")
+    Tester("Staging.lerp(-22, -13, 0.0).toFloat", Some("Float = -22.0"))
+    Tester("Staging.lerp(-22, -13, 0.3).toFloat", Some("Float = -19.3"))
+    Tester("Staging.lerp(-22, -13, 0.5).toFloat", Some("Float = -17.5"))
+    Tester("Staging.lerp(-22, -13, 1.0).toFloat", Some("Float = -13.0"))
   //W}}}
   //W
   //WThe inverse of `norm`; yields a number in the range _low_ -- _high_
   //Wwhich corresponds to the position of _value_ in the range 0.0 -- 1.0.
   //W
   }
-
-  def stripCrLfs(str: String) = str.replaceAll("\r?\n", "")
 }
 
--- KojoEnv/test/unit/src/net/kogics/kojo/staging/StagingTestBase.scala
+++ KojoEnv/test/unit/src/net/kogics/kojo/staging/StagingTestBase.scala
@@ -86,6 +86,51 @@ class StagingTestBase extends KojoTestBase {
 
   def stripCrLfs(str: String) = str.replaceAll("\r?\n", "")
 
+  val CL = java.awt.geom.PathIterator.SEG_CLOSE   // 4
+  val CT = java.awt.geom.PathIterator.SEG_CUBICTO // 3
+  val LT = java.awt.geom.PathIterator.SEG_LINETO  // 1
+  val MT = java.awt.geom.PathIterator.SEG_MOVETO  // 0
+  val QT = java.awt.geom.PathIterator.SEG_QUADTO  // 2
+  val fmt = "%g"
+  def segmentToString(t: Int, coords: Array[Double]) = t match {
+    case MT =>
+      "M" + (fmt format coords(0)) + "," + (fmt format coords(1)) + " "
+    case LT =>
+      "L" + (fmt format coords(0)) + "," + (fmt format coords(1)) + " "
+    case QT =>
+      "Q" + (fmt format coords(0)) + "," + (fmt format coords(1)) + " " +
+            (fmt format coords(2)) + "," + (fmt format coords(3)) + " "
+    case CT =>
+      "C" + (fmt format coords(0)) + "," + (fmt format coords(1)) + " " +
+            (fmt format coords(2)) + "," + (fmt format coords(3)) + " " +
+            (fmt format coords(4)) + "," + (fmt format coords(5)) + " "
+    case CL =>
+      "z "
+  }
+
+  def pathIteratorToString(pi: java.awt.geom.PathIterator) = {
+    var res = new StringBuffer
+    while (!pi.isDone) {
+      pi.next
+      val coords = Array[Double](0, 0, 0, 0, 0, 0)
+      val t = pi.currentSegment(coords)
+      res.append(segmentToString(t, coords))
+    }
+    res.toString
+  }
+
+  def pathReferenceToString(pr: java.awt.geom.Path2D) = {
+    val at = new java.awt.geom.AffineTransform
+    val pi = pr.getPathIterator(at)
+    pathIteratorToString(pi)
+  }
+  def pathData(polyLine: kgeom.PolyLine) = {
+    pathReferenceToString(polyLine.polyLinePath)
+  }
+  def pathData(ppath: PPath) = {
+    pathReferenceToString(ppath.getPathReference)
+  }
+
   def makeString(pnode: PNode) = {
     val x = pnode.getX.round + 1
     val y = pnode.getY.round + 1
@@ -96,10 +141,12 @@ class StagingTestBase extends KojoTestBase {
       "PPoint(" + x + "," + y + ")"
     }
     else if (pnode.isInstanceOf[kgeom.PolyLine]) {
-      "PolyLine(" + (x + 1) + "," + (y + 1) + ")"
+      "PolyLine(" + (x + 1) + "," + (y + 1) + " " +
+        pathData(pnode.asInstanceOf[kgeom.PolyLine]) + ")"
     }
     else if (pnode.isInstanceOf[PPath]) {
-      "PPath(" + x + "," + y + ")"
+      "PPath(" + x + "," + y + " " +
+        pathData(pnode.asInstanceOf[PPath]) + ")"
     }
     else pnode.toString
   }
