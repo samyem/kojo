f68a858961bf6e48949fddefe1704760a2cbf00c
Lalit Pant <pant.lalit@gmail.com>
2012-02-08 13:33:57 +0530

    Add Protractor tool.

--- KojoEnv/src/net/kogics/kojo/picture/package.scala
+++ KojoEnv/src/net/kogics/kojo/picture/package.scala
@@ -39,4 +39,75 @@ package object picture {
     }
     VPics(lb.toList)
   }
+  
+  def protractor(camScale: Double) = {
+    val r = 90 / camScale
+    def num(n: Int) = Pic { t =>
+      import t._
+      setPenFontSize(10)
+      write(n)
+    }
+    def cross = Pic { t =>
+      import t._
+      def line() {
+        forward(r/20)
+        penUp()
+        back(r/10)
+        penDown()
+        forward(r/20)
+      }
+      line()
+      right()
+      line()
+    }
+    def line = Pic { t =>
+      import t._
+      right()
+      penUp()
+      forward(r/4)
+      penDown()
+      forward(3*r/4)
+    }
+    def slice = Pic { t =>
+      import t._
+      right()
+      penUp()
+      forward(r/4)
+      penDown()
+      forward(3*r/4)
+      left()
+      for (i <- 1 to 50) {
+        forward(2 * math.Pi * r / 360 * 0.2)
+        left(0.2)
+      }
+    }
+    def prot(n: Int): Picture = {
+      def angletext(n: Int) = if (n >= 90) {
+        trans(1.05*r, 8*r/100) -> num(180-n)
+      }
+      else {
+        trans(1.25*r, -8*r/100) * rot(180) -> num(180-n)
+      }
+    
+      if (n == 0) {
+        GPics(
+          line,
+          angletext(n),
+          cross
+        )
+      }
+      else {
+        GPics(
+          slice,
+          angletext(n),
+          rot(10) -> prot(n-10)
+        )
+      }
+    }
+    val p = opac(-0.5) * stroke(Color.black) -> prot(180)
+    p.onMouseDrag { (x, y) =>
+      p.setPosition(x, y)
+    }
+    p
+  }
 }
