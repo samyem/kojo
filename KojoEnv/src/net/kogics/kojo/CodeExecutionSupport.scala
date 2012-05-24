38fe6830fbed7745ef293b360a5a90ee2ea1334d
Lalit Pant <pant.lalit@gmail.com>
2012-05-24 20:28:32 +0530

    Tweak welcome message and drawing canvas context menu text - to take scroll-wheel zooming into account.

--- KojoEnv/src/net/kogics/kojo/CodeExecutionSupport.scala
+++ KojoEnv/src/net/kogics/kojo/CodeExecutionSupport.scala
@@ -175,7 +175,7 @@ class CodeExecutionSupport private extends core.CodeCompletionSupport with Manip
     |* To use code completion and see online help ->  Press Ctrl+Space within the Script Editor
     |* To interactively manipulate program output ->  Ctrl+Click on numbers within the Script Editor
     |* To access the context actions for a window ->  Right-Click on the window to bring up its context menu
-    |* To Pan or Zoom the Drawing Canvas          ->  Drag or Shift-Drag with the left mouse button
+    |* To Pan or Zoom the Drawing Canvas          ->  Drag the left mouse button or Roll the mouse wheel
     |  * To reset Pan and Zoom levels             ->  Use the Drawing Canvas context menu
     |""".stripMargin
     
